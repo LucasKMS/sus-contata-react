564ba2cd7be8919d9593a37cfdaf3387c99f545b
Lucas <lucas.sampaio9@fatec.sp.gov.br>
2024-06-18 22:51:49 -0300

    Deletando Agendamento

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
-import CadastroUBS from '../components/CadastroUbs';
 import CadastroNovoUsuario from '../components/CadastroNovoUsuario';
+import CadastroPaciente from './CadastroPacientes';
 
 function Cadastro() {
   const [activeComponent, setActiveComponent] = useState(null);
 
   const renderComponent = () => {
     switch (activeComponent) {
-      case 'CadastroUBS':
-        return <CadastroUBS />;
       case 'CadastroNovoUsuario':
         return <CadastroNovoUsuario />;
+        case 'CadastroPaciente':
+        return <CadastroPaciente />;
       default:
         return null;
     }
@@ -21,19 +21,21 @@ function Cadastro() {
     <div className="d-flex flex-column align-items-center space-between">
       <h2 className="mb-3">Selecionar o tipo de cadastro:</h2>
         <div className="d-flex flex-row align-items-center gap-2">
-            <Button
-                variant="primary"
-                className="mb-2 w-10"
-                onClick={() => setActiveComponent('CadastroUBS')}
-                > Cadastrar Nova UBS
-            </Button>
 
             <Button
-                variant="secondary"
+                variant="primary"
                 className="mb-2"
                 onClick={() => setActiveComponent('CadastroNovoUsuario')}
                 >
                 Cadastrar Novo Paciente
+            </Button>
+
+            <Button
+                variant="success"
+                className="mb-2"
+                onClick={() => setActiveComponent('CadastroPaciente')}
+                >
+                Cadastrar Novo Agendamento
             </Button>
         </div>
       {renderComponent()}
---
+++ src/components/CadastroPacientes.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import { Form, Button, Col, Row, Container } from 'react-bootstrap';
+import axios from 'axios';
+
+const PacienteSchema = new Schema({
+  cpf: { type: String, required: true, unique: true },
+  nomeCompleto: { type: String, required: true },
+  dataNascimento: { type: Date, required: true },
+  endereco: { type: EnderecoSchema, required: true },
+  email: { type: String },
+  telefones: { type: TelefoneSchema, _id: false },
+  contatosAdicionais: {
+      contato1: { type: ContatoAdicionalSchema, _id: false },
+      contato2: { type: ContatoAdicionalSchema, _id: false }
+  },
+  encaixe: { type: Boolean },
+  tipoUsuario: {
+      type: String,
+      enum: ['adm', 'paciente', 'medico'],
+      required: true,
+      default: 'paciente'
+  }
+}, { timestamps: true })
+
+const [cpf, setCpf] = useState('')
+const [nomeCompleto, setNomeCompleto] = useState('')
+const [dataNascimento, setDataNascimento] = useState()
+
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+}
+
+const CadastroPaciente = () => {
+  return (
+    <Container className="d-flex flex-column align-items-center">
+      <h2 className="mb-3">Novo Agendamento</h2>
+
+      <Form action="agendamento" method="post" className="w-100 d-flex flex-column align-items-center">
+        <Form.Group as={Col} md="6" className="mb-3">
+          <fieldset className="form-control">
+            <legend>Selecionar Cliente</legend>
+            
+            <Form.Label htmlFor="clienteCPF" className="col-form-label">Digite o CPF do cliente:</Form.Label>
+            <Form.Control 
+            type="text" 
+            name="clienteCPF" 
+            id="clienteCPF" 
+            onChange={handleChange}
+            />
+
+            <Form.Label htmlFor="clienteNome" className="col-form-label">Nome:</Form.Label>
+            <Form.Control 
+            type="text" 
+            name="clienteNome" 
+            id="clienteNome" 
+            disabled 
+            readOnly />
+
+            <Form.Label htmlFor="dataNascimento" className="col-form-label">Data de nascimento:</Form.Label>
+            <Form.Control 
+            type="date" 
+            id="dataNascimento"
+            disabled 
+            readOnly />
+
+            <Form.Label htmlFor="clienteEndereco" className="col-form-label">Endereço:</Form.Label>
+            <Form.Control 
+            as="textarea" 
+            name="clienteEndereco" 
+            id="clienteEndereco" 
+            disabled 
+            readOnly 
+            />
+          </fieldset>
+        </Form.Group>
+
+        <Form.Group as={Col} md="6">
+          <fieldset className="form-control">
+            <legend>Solicitar Agendamento</legend>
+            
+            <Form.Label htmlFor="selUnidade" className="col-form-label">Unidade:</Form.Label>
+            <Form.Control 
+            as="select" 
+            name="selUnidade"
+            id="selUnidade"
+            >
+              <option selected>Selecionar UBS</option>
+              <option value="1">UBS - Vila Nova</option>
+              <option value="2">UBS - Vila Garcia</option>
+              <option value="3">UBS - Itapeva</option>
+              <option value="4">UBS - Barra Funda</option>
+              <option value="5">UBS - Novo Mundo</option>
+              <option value="6">UBS - Vila Amorim</option>
+              <option value="7">UBS - Jardim Archila</option>
+              <option value="8">UBS - Jardim Serrano</option>
+              <option value="9">UBS - Jardim Clarice</option>
+              <option value="10">UBS - Parque Bela Vista</option>
+              <option value="11">UBS - Rio Acima</option>
+              <option value="12">ESF - Green Valley</option>
+              <option value="13">ESF - Jardim Tatiana</option>
+              <option value="14">ESF - PROMORAR</option>
+            </Form.Control>
+
+            <Row>
+              <Col md="6">
+                <Form.Label htmlFor="data" className="col-form-label">Data:</Form.Label>
+                <Form.Control 
+                type="date" 
+                name="data" 
+                id="data" 
+                />
+              </Col>
+
+              <Col md="6">
+                <Form.Label htmlFor="horario" className="col-form-label">Horario:</Form.Label>
+                <Form.Control 
+                type="time" 
+                name="horario" 
+                id="horario" 
+                />
+              </Col>
+            </Row>
+
+            <Form.Label htmlFor="tipoAtendimento" className="col-form-label">Tipo atendimento:</Form.Label>
+            <Form.Control 
+            as="select" 
+            name="tipoAtendimento" 
+            id="tipoAtendimento"
+            >
+              <option selected>Selecionar tipo de atendimento</option>
+              <option value="Consulta">Consulta</option>
+              <option value="Exame">Exame</option>
+              <option value="Procedimento">Procedimento</option>
+              <option value="Retorno">Retorno</option>
+            </Form.Control>
+
+            <Form.Label htmlFor="especialidade" className="col-form-label">Especialidade:</Form.Label>
+            <Form.Control 
+            type="text" 
+            id="especialidade" />
+          </fieldset>
+        </Form.Group>
+
+        <Button type="submit" className="mt-3" variant="primary">Agendar</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default CadastroPaciente;
